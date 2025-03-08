503ea4fc74c496919da33564fdd4e1bac8ccb2f1
siddhartha <91248720+sidblue88@users.noreply.github.com>
2025-03-08 16:05:33 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "azurerm" {
+  features {}
+}
