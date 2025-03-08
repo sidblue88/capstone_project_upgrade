256343be9f5eb45a6acd31fa5f22a8e1d2583079
siddhartha <91248720+sidblue88@users.noreply.github.com>
2025-03-08 15:59:04 +0530

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id           = aws_vpc.main.id
+  cidr_block       = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count       = length(var.private_subnets)
+  vpc_id      = aws_vpc.main.id
+  cidr_block  = var.private_subnets[count.index]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
