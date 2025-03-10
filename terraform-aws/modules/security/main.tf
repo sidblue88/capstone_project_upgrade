497c12957dd71563c3f693d5ec5de883d235c58e
siddhartha <91248720+sidblue88@users.noreply.github.com>
2025-03-10 12:30:07 +0530

    Create main.tf

---
+++ terraform-aws/modules/security/main.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
+
+resource "aws_key_pair" "ec2_key" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
